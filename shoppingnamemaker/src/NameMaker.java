853cf22188c4496c857a9a5f551fdbdb68fb4443
Jennamaslovich <108747993+Jennamaslovich@users.noreply.github.com>
2022-08-23 12:34:32 -0400

    shoppingcart and NameMaker

---
+++ shoppingnamemaker/src/NameMaker.java
@@ -0,0 +1,15 @@
+
+public class NameMaker {
+    
+    public static void main(String args[])
+    {
+        String firstName="Jenna",
+                middleName="Marie",
+                lastName="Maslovich",
+                fullName;
+
+        fullName=firstName+" "+middleName+" "+lastName;
+        System.out.println(fullName);
+    }
+    
+}
