3dcfc2da0af122ac4e114983f7bb6eb66862da1c
Jennamaslovich <108747993+Jennamaslovich@users.noreply.github.com>
2022-08-23 14:45:41 -0400

    Rockpaperscissor

---
+++ rockpapers/src/RockPaperScissor.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class RockPaperScissor {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int chance = rand.nextInt(3);
+
+        if (chance ==0) {
+            System.out.println("rock");
+        }
+        if (chance ==1) {
+            System.out.println("paper");
+        }
+
+        if (chance ==2) {
+            System.out.println("scissor");
+        }
+    }
+}
+        
+
+
